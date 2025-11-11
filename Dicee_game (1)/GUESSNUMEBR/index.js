35c379354a572ed9cafdbb507fe638f68875deb4
Lara452 <larajovic94@gmail.com>
2025-11-11 12:01:34 +0100

    sg
    grd

---
+++ Dicee_game (1)/GUESSNUMEBR/index.js
@@ -0,0 +1,19 @@
+function randNumber(){
+    return Math.floor(Math.random()*100)+1;
+
+}
+let randNumber=randNumber();
+let pokusaji=0;
+let input=document.getElememtById("guess");
+let msg =document.getElememtById("msg");
+let attempts=document.getElementById("attempts");
+
+function updateAttempts(){
+
+}
+function checkNumber(){
+
+}
+function checkNumber(){
+    
+}
--- Dicee_game (1)/index.js
+++ Dicee_game (1)/index.js
@@ -1,4 +1,30 @@
 let randomNumber1 = Math.floor(Math.random()*6)+1;
-//console.log(randomNumber1);
-let randomImageSource1="images/dice"+randomNumber1 + ".png";
-document.querySelectorAll("img")[0].setAttribute("src",randomImageSource1);
+let randomImageSource1 = "images/dice" + randomNumber1 + ".png";
+document.querySelectorAll("img")[0].setAttribute("src", randomImageSource1);
+
+let randomNumber2 = Math.floor(Math.random()*6)+1;
+let randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+let randomNumber3 = Math.floor(Math.random()*6)+1;
+let randomImageSource3 = "images/dice" + randomNumber3 + ".png";
+document.querySelectorAll("img")[2].setAttribute("src", randomImageSource3);
+
+let randomNumber4 = Math.floor(Math.random()*6)+1;
+let randomImageSource4 = "images/dice" + randomNumber4 + ".png";
+document.querySelectorAll("img")[3].setAttribute("src", randomImageSource4);
+
+
+
+let p1 = randomNumber1 + randomNumber2;
+let p2 = randomNumber3 + randomNumber4;
+
+if (p1 > p2){
+    document.querySelectorAll("h1")[0].innerHTML = "🎉Player 1 Wins!🎉";
+}
+else if (p1 < p2){
+    document.querySelectorAll("h1")[0].innerHTML = "🎉Player 2 Wins!🎉";
+}
+else{
+    document.querySelectorAll("h1")[0].innerHTML = "DRAW😒";
+}

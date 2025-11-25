addf0cea957126925593083046184e3489214145
MIRKOSKRINJAR <mirko.skrinjar08@gmail.com>
2025-11-25 10:34:58 +0100

    lčlč

--- Dicee_game (1)/GUESSNUMEBR/index.js
+++ Dicee_game (1)/GUESSNUMEBR/index.js
@@ -1,19 +1,56 @@
-function randNumber(){
-    return Math.floor(Math.random()*100)+1;
 
+
+function randNumber() {
+    return Math.floor((Math.random() * 100) + 1);
 }
-let randNumber=randNumber();
-let pokusaji=0;
-let input=document.getElememtById("guess");
-let msg =document.getElememtById("msg");
-let attempts=document.getElementById("attempts");
 
-function updateAttempts(){
+let random_Number = randNumber();
+let pokušaji = 0;
 
+let Input = document.getElementById("guess");
+let Msg = document.getElementById("msg");
+let Attempts = document.getElementById("attempts");
+
+function updateAttempts() {
+    Attempts.innerHTML = "Pokušaji: " + pokušaji;
 }
-function checkNumber(){
 
+updateAttempts();
+
+function checkNumber(event) {
+    event.preventDefault(); 
+    let value = parseInt(Input.value);
+
+    if (isNaN(value) || value < 1 || value > 100) {
+        Msg.innerHTML = "Unesi broj između 1 i 100";
+        Msg.style.color = "Crimson";
+        return;
+    }
+
+    pokušaji++;
+    updateAttempts();
+
+    if (value === random_Number) {
+        Msg.innerHTML = "Pogodak, broj je " + random_Number;
+        Msg.style.color = "green";
+
+    } else if (value < random_Number) {
+        Msg.innerHTML = "Prenisko";
+        Msg.style.color = "Orange";
+
+    } else {
+        Msg.innerHTML = "Previsoko";
+        Msg.style.color = "Orange";
+    }
 }
-function checkNumber(){
+
+function newGame() {
+    random_Number = randNumber();
+    pokušaji = 0;
+    Input.value = "";
+    Msg.innerHTML = "NOVA IGRA";
+    Msg.style.color = "black";
+    updateAttempts();
+    Input.focus (); 
     
 }
